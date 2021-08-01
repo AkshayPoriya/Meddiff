6bc649146c0cb274d7e86ce314e0e502db8fcbba
AkshayPoriya <akshayporiya1234@gmail.com>
2021-08-02 03:40:32 +0530

    Added Solution of Problem #5 using Asp.Net MVC. Attached image to understand the working of web application.

---
+++ Problem 5/Student Records/Student Records/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using Student_Records.DataOperations;
+using Student_Records.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Records.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(StudentRepository.studentList);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            Student student = StudentRepository.GetStudent(id);
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                StudentRepository.AddStudent(student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(StudentRepository.GetStudent(id));
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Student student)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                StudentRepository.UpdateStudent(id, student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student student = StudentRepository.GetStudent(id);
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Student student)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                StudentRepository.DeleteStudent(id);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Problem 5/Student Records/Student Records/DataOperations/StudentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Student_Records.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Student_Records.DataOperations
+{
+    public class StudentRepository
+    {
+        public static int id = 1;
+        public static List<Student> studentList = new List<Student>();
+
+        public static void AddStudent(Student studentModel)
+        {
+            studentModel.Id = id++;
+            studentList.Add(studentModel);
+        }
+
+        public static Student GetStudent(int id)
+        {
+            return studentList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static bool UpdateStudent(int id, Student studentModel)
+        {
+            var student = studentList.FirstOrDefault(x => x.Id == id);
+            if (student != null)
+            {
+                student.Name = studentModel.Name;
+                student.RollNumber = studentModel.RollNumber;
+                student.Age = studentModel.Age;
+                student.Gender = studentModel.Gender;
+                return true;
+            }
+            return false;
+        }
+
+        public static void DeleteStudent(int id)
+        {
+            Student student = studentList.FirstOrDefault(x => x.Id == id);
+            if (student != null)
+            {
+                studentList.Remove(student);
+            }
+        }
+    }
+}
