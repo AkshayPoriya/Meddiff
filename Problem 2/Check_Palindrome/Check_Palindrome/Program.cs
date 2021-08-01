8091dbdc8a9019857b1bb56a488de6cdf1506b39
AkshayPoriya <akshayporiya1234@gmail.com>
2021-08-02 00:42:31 +0530

    Added Solution of Problem #2, please find Progrm.cs

---
+++ Problem 2/Check_Palindrome/Check_Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Check_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter List of space seprated words");
+            string[] words = Console.ReadLine().Split(' ');
+            foreach(string str in words)
+            {
+                string word = str.ToLower();
+                int length = word.Length;
+                bool isPalindrome = true;
+                for (int i = 0; i < length / 2; i++)
+                {
+                    if (word[i] != word[length - 1 - i])
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome)
+                {
+                    Console.WriteLine("\n" + word + " is a Palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine("\n" + word + " is not a Palindrome!");
+                }
+            }
+        }
+    }
+}
